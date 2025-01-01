2b96e4d0d30ff2882992d721cb525cd758e60dbb
Swostika <115869284+swostikaa99@users.noreply.github.com>
2025-01-01 16:54:15 +0545

    Update align.tsx
    Signed-off-by: Swostika <115869284+swostikaa99@users.noreply.github.com>

--- components/pagination/demo/align.tsx
+++ components/pagination/demo/align.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Pagination } from 'antd';
+import React, { useState } from 'react';
+import { Pagination, Select } from 'antd';
 
-const App: React.FC = () => (
-  <>
-    <Pagination align="start" defaultCurrent={1} total={50} />
-    <br />
-    <Pagination align="center" defaultCurrent={1} total={50} />
-    <br />
-    <Pagination align="end" defaultCurrent={1} total={50} />
-  </>
-);
+const { Option } = Select;
+
+const App: React.FC = () => {
+  const [alignment, setAlignment] = useState<'start' | 'center' | 'end'>('start');
+
+  const handleAlignmentChange = (value: 'start' | 'center' | 'end') => {
+    setAlignment(value);
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <Select
+        value={alignment}
+        onChange={handleAlignmentChange}
+        style={{ width: 120, marginBottom: 20 }}
+      >
+        <Option value="start">Start</Option>
+        <Option value="center">Center</Option>
+        <Option value="end">End</Option>
+      </Select>
+      <br />
+      <Pagination align={alignment} defaultCurrent={1} total={50} />
+    </div>
+  );
+};
 
 export default App;
